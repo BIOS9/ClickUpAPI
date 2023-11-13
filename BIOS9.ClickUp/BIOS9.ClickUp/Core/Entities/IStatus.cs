7778ed990a7a30d9f66a03e5096c4966a9698bbf
NightFish <NightFish.net@gmail.com>
2023-11-13 15:01:45 +1300

    new: add task status abstractions

---
+++ BIOS9.ClickUp/BIOS9.ClickUp/Core/Entities/IStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace BIOS9.ClickUp.Core.Entities;
+
+public interface IStatus
+{
+    enum StatusType
+    {
+        Open,
+        Done,
+        Closed
+    }
+    
+    string Value { get; }
+    string Color { get; }
+    int OrderIndex { get; }
+    StatusType Type { get; }
+}
